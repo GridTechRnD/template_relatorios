2ac6e2f895fd2151f65c5e1c8f8b0bd4d5051610
Vanderlei Jordão de Almeida Jr <vanderlei.junior@gridtech.com.br>
2025-06-02 15:03:07 -0300

    Remoção de base64 do template

--- app/api.py
+++ app/api.py
@@ -8,27 +8,30 @@ from model import Model
 from PIL import Image
 from fpdf import FPDF
 from playwright.async_api import async_playwright
+from base_64 import HEXING_LOGO, TAHOMA, CELESC_LOGO
 
-
-app = FastAPI(name="Gerador de relatório", version="1.0.0",
-              root_path="/html2pdf", swagger_ui_parameters={"syntaxHighlight": True})
+app = FastAPI(
+    name="Gerador de relatório",
+    version="1.0.0",
+    root_path="/html2pdf",
+    swagger_ui_parameters={"syntaxHighlight": True},
+)
 
 
 async def html_pdf(html_string, pdf_path, img_path):
     async with async_playwright() as p:
-        browser = await p.chromium.launch(args=['--no-sandbox'])
+        browser = await p.chromium.launch(args=["--no-sandbox"])
         page = await browser.new_page(
-            viewport={"width": 794, "height": 1123},
-            device_scale_factor=5
+            viewport={"width": 794, "height": 1123}, device_scale_factor=5
         )
         await page.set_content(html_string)
-        page.wait_for_load_state('networkidle')
+        page.wait_for_load_state("networkidle")
         await page.evaluate("document.fonts.ready")
         screenshot_bytes = await page.screenshot(full_page=True)
         await browser.close()
 
     image = Image.open(io.BytesIO(screenshot_bytes))
-    image_rgb = image.convert('RGB')
+    image_rgb = image.convert("RGB")
     image_rgb.save(img_path)
 
     pdf = FPDF(unit="pt", format=[image.width, image.height])
@@ -50,24 +53,38 @@ async def build(item: Model):
                 base64.b64decode(i)
             except Exception:
                 raise Exception("The blob is not a base64 string")
-    
-    item["enterprise_logo"] = f'<img src="data:image/png;base64, {item["enterprise_logo"]}" width="84" height="34"/>'
-    item["images"] = '\n'.join([f'<img src="data:image/png;base64, {i}" />' for i in item["images"]])
+
+    item["images"] = "\n".join(
+        [f'<img src="data:image/png;base64, {i}" />' for i in item["images"]]
+    )
+    enterprise_logo = ""
+    if item["enterprise_logo"] and item["enterprise_logo"] == "celesc":
+        enterprise_logo = (
+            f'<img src="data:image/png;base64, {CELESC_LOGO}" width="84" height="34"/>'
+        )
+
+    item.update(
+        {
+            "HEXING_LOGO": f'<img src="data:image/png;base64, {HEXING_LOGO}" width="69" height="61"/>'.strip(),
+            "TAHOMA": TAHOMA.strip(),
+            "enterprise_logo": enterprise_logo.strip(),
+        }
+    )
+
     html = HTML.format(**item)
     pdf_path = "/media/temp.pdf"
     img_path = "/media/temp_image.jpg"
     await html_pdf(html, pdf_path, img_path)
 
     response = FileResponse(
-        pdf_path,
-        media_type="application/pdf",
-        filename="documento.pdf"
+        pdf_path, media_type="application/pdf", filename="documento.pdf"
     )
 
     from fastapi import BackgroundTasks
 
     def cleanup():
         os.remove(pdf_path)
+
     response.background = BackgroundTasks()
     response.background.add_task(cleanup)
 
